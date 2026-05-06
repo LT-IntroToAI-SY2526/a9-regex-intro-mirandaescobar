4d44bd82639c9b92ee58a0353c36634619e04c5f
Miranda Escobar <mdescobar@cps.edu>
2026-05-06 10:45:40 -0500

    successful a9 pt 1

--- a9_part_I.py
+++ a9_part_I.py
@@ -8,8 +8,8 @@ import re
 # problem 1
 # should extract a match where the first group is the month, the second group the day
 # and the third group the year
-date_string = "November 9, 1982"
-pat = re.compile("(\w+) (\d) (\d{4})", re.IGNORECASE)
+date_string = "January 16, 2008"
+pat = re.compile(r"(\w+) (\d{1,2}), (\d{4})", re.IGNORECASE)
 date_matches = pat.match(date_string)
 
 # problem 2
@@ -22,7 +22,7 @@ address_matches = pat.match(address_string)
 # problem 3
 # should match all hashtags
 tweet_string = "hi everyone! #cs #python #LT #champions"
-pat = re.compile("(#)", re.IGNORECASE)
+pat = re.compile("#(\w+)", re.IGNORECASE)
 hashtag_matches = pat.findall(tweet_string)
 
 # until you uncomment any code line below you'll get an EOF linting error feel free to
@@ -30,32 +30,32 @@ hashtag_matches = pat.findall(tweet_string)
 if __name__ == "__main__":
     print("<<<<< Date Problem >>>>>\n")
     # uncomment the following prints to see date results and asserts to test
-    # print(f"month is: {date_matches.group(1)}!") # should print "month is: November"
-    # print(f"day is: {date_matches.group(2)}!")   # should print "day is: 9"
-    # print(f"year is: {date_matches.group(3)}!")  # should print "year is: 1982"
-    # assert date_matches.group(1) == 'November', "Incorrect month"
-    # assert date_matches.group(2) == '9', "Incorrect day"
-    # assert date_matches.group(3) == '1982', "Incorrect year"
+    print(f"month is: {date_matches.group(1)}!") # should print "month is: January"
+    print(f"day is: {date_matches.group(2)}!")   # should print "day is: 16"
+    print(f"year is: {date_matches.group(3)}!")  # should print "year is: 2008"
+    assert date_matches.group(1) == 'January', "Incorrect month"
+    assert date_matches.group(2) == '16', "Incorrect day"
+    assert date_matches.group(3) == '2008', "Incorrect year"
     # print('\n<<<< Date extraction tests passed >>>>\n')
 
     print("<<<<< Address Problem >>>>>\n")
     # uncomment the following prints to see results and asserts to test
-    # print(f'number is: {address_matches.group("number")}!') # should print "number is: 2501"
-    # print(f'street is: {address_matches.group("street")}!') # should print "street is: Addison Street"
-    # print(f'city is: {address_matches.group("city")}!')     # should print "city is: Chicago"
-    # print(f'state is: {address_matches.group("state")}!')   # should print "state is: IL"
-    # print(f'zip is: {address_matches.group("zip")}!')       # should print "zip is: 60618"
-    # assert address_matches.group('number') == '2501', "Incorrect address number"
-    # assert address_matches.group('street') == 'Addison Street', "Incorrect street"
-    # assert address_matches.group('city') == 'Chicago', "Incorrect city"
-    # assert address_matches.group('state') == 'IL', "Incorrect state"
-    # assert address_matches.group('zip') == '60618', "Incorrect zip"
+    print(f'number is: {address_matches.group("number")}!') # should print "number is: 2501"
+    print(f'street is: {address_matches.group("street")}!') # should print "street is: Addison Street"
+    print(f'city is: {address_matches.group("city")}!')     # should print "city is: Chicago"
+    print(f'state is: {address_matches.group("state")}!')   # should print "state is: IL"
+    print(f'zip is: {address_matches.group("zip")}!')       # should print "zip is: 60618"
+    assert address_matches.group('number') == '2501', "Incorrect address number"
+    assert address_matches.group('street') == 'Addison Street', "Incorrect street"
+    assert address_matches.group('city') == 'Chicago', "Incorrect city"
+    assert address_matches.group('state') == 'IL', "Incorrect state"
+    assert address_matches.group('zip') == '60618', "Incorrect zip"
     # print('\n<<<< Address extraction tests passed >>>>\n')
 
     print("<<<<< Hashtag Problem >>>>>\n")
     # uncomment the following prints to see results and asserts to test
-    # print(f"hashtags are: {hashtag_matches}") # should be ['cs', 'python', 'LT', 'champions']"
-    # assert hashtag_matches == ['cs', 'python', 'LT', 'champions'], "Incorrect hashtags"
+    print(f"hashtags are: {hashtag_matches}") # should be ['cs', 'python', 'LT', 'champions']"
+    assert hashtag_matches == ['cs', 'python', 'LT', 'champions'], "Incorrect hashtags"
     # print('\n<<<< Hashtag extraction tests passed >>>>\n')
 
     # print('\n<<<< All tests passed! >>>>')
